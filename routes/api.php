95d800661ee65f969c2eba6d339b0a67fa1b6aa0
rohmatsyam <rohmatsyam@yahoo.com>
2022-03-23 01:24:14 +0700

    "auth sanctum with email verify"

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 
-Route::apiResource("/animal", App\Http\Controllers\Api\AnimalController::class);
-Route::apiResource("/cage", App\Http\Controllers\Api\CageController::class);
+Route::get('email/verify/{id}', [App\Http\Controllers\Api\AuthController::class, 'verify'])->name('verification.verify');
+Route::post('email/resend', [App\Http\Controllers\Api\AuthController::class, 'resend'])->name('verification.resend');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource("/animal", App\Http\Controllers\Api\AnimalController::class);
+    Route::apiResource("/cage", App\Http\Controllers\Api\CageController::class);
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+});
